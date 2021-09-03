eb3682da068c9d878ad1313c18e79f6d2497666e
qiamfafafilm <89238281+qiamfafafilm@users.noreply.github.com>
2021-09-03 11:39:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ data = [
 
 @app.route('/')
 def hello():
-    return "Hello Flask-Herok"
+    return "Hello กาญจนา แสงจันทร์ เลขที่ 23 ชั้น ม.4/7"
 
 @app.route('/api', methods=['GET'])
 def get_api():
